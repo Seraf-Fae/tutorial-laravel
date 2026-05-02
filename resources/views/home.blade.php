768bcdfe4baaaab953e57aecc50be373cda0e10d
Seraf Fae <seraf.fae@gmail.com>
2026-05-02 15:06:04 -0400

    Primer Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>
+        Hola
+    </x-slot:title>
+
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">Pagina de Tutorial!</h1>
+                    <p class="mt-4 text-base-content/60">Estamos haciendo el tutorial a ver que onda</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
