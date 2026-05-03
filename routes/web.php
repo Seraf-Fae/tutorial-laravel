1c8b178035f786430a4489d3f1c8b8bd58158011
Seraf Fae <seraf.fae@gmail.com>
2026-05-03 15:45:17 -0400

    Tutorial completado

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 use Illuminate\Support\Facades\Route;
 
 # Chirp Based Routes
@@ -24,3 +26,14 @@ Route::middleware('auth')->group(function () {
     Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
     Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 });
+
+Route::view('/user/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', Login::class)
+    ->middleware('guest');
+
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
